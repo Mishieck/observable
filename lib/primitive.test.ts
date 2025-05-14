255fc45e518a640fb2463d2574ecb54901b754fe
Mishieck <mishieckmwale@gmail.com>
2025-05-14 18:01:01 +0200

    feat(primitive): add a new class for deriving values from other values

--- lib/primitive.test.ts
+++ lib/primitive.test.ts
@@ -1,5 +1,10 @@
 import { describe, it } from "@std/testing";
-import { Primitive, type PrimitiveObserver } from "./primitive.ts";
+import {
+  DerivedPrimitive,
+  Deriver,
+  Primitive,
+  type PrimitiveObserver,
+} from "./primitive.ts";
 import { expect } from "@std/expect";
 
 describe("Primitive", () => {
@@ -22,5 +27,24 @@ describe("Primitive", () => {
     expect(value).toEqual(false);
     expect(primitive.current).toEqual(false);
     expect(primitive.current).toEqual(false);
+  });
+});
+
+describe("DerivedPrimitive", () => {
+  it("should handle observation", () => {
+    const primitive = new Primitive(0);
+
+    const isEven: Deriver<number, boolean> = (value): boolean =>
+      value % 2 === 0;
+
+    const parityPrimitive = new DerivedPrimitive(primitive, isEven);
+
+    expect(parityPrimitive.current).toEqual(true);
+    primitive.current = 1;
+    expect(parityPrimitive.current).toEqual(false);
+    primitive.current = 2;
+    expect(parityPrimitive.current).toEqual(true);
+    parityPrimitive.current = false;
+    expect(parityPrimitive.current).toEqual(true);
   });
 });
--- lib/primitive.ts
+++ lib/primitive.ts
@@ -6,6 +6,9 @@ export type PrimitiveEvent = "update";
 /** The observer of a primitive observable value. */
 export type PrimitiveObserver<Value> = (value: Value) => void;
 
+/** A function that derives one value from another. */
+export type Deriver<Value, DerivedValue> = (value: Value) => DerivedValue;
+
 export class Primitive<Value>
   extends Observable<PrimitiveEvent, Record<PrimitiveEvent, Value>> {
   /** The current primitive value. This is the value that gets observed. */
@@ -57,5 +60,34 @@ export class Primitive<Value>
   /** Emits the 'update' event to `PrimitiveObservers`. */
   notify(value: Value) {
     this.notifyObservers("update", value);
+  }
+}
+
+/** Derives a primitive value from another primitive value. */
+export class DerivedPrimitive<BaseValue, DerivedValue>
+  extends Primitive<DerivedValue> {
+  #baseValue: Primitive<BaseValue>;
+  #derive: Deriver<BaseValue, DerivedValue>;
+
+  constructor(
+    baseValue: Primitive<BaseValue>,
+    derive: Deriver<BaseValue, DerivedValue>,
+  ) {
+    super(derive(baseValue.current));
+    this.#baseValue = baseValue;
+    this.#derive = derive;
+    this.#baseValue.observe((value) => this.notify(this.#derive(value)));
+  }
+
+  /**
+   * The derived current value. The `Deriver` is used to derive the current
+   * value from the `Primitive` value used.
+   */
+  override get current(): DerivedValue {
+    return this.#derive(this.#baseValue.current);
+  }
+
+  /** Since the value is derived. Setting this value has no effect. */
+  override set current(_value: DerivedValue) {
   }
 }
